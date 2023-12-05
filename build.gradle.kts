9c6291915568cbce466c1d8d2f12130859bcc24f
Tatyana Moskvitina <moskvitinatanya429@gmail.com>
2023-12-05 19:11:50 +0200

    packages controllers,dao,services added. Templates and static packages added in Resources. BookShop.app class added

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,17 @@ plugins {
     id("java")
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.app"
+version = "0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.0")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.2.0")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
